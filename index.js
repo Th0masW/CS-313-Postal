6aa80dd506c40ea70fa26b3f58fa1f9820b5ca45
Thomas Whitelock <whi16060@byui.edu>
2018-11-20 16:25:56 -0500

    finished

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  
+
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
@@ -28,19 +28,52 @@ express()
     }
   })
   
-  .get('/cool', (req, res) => res.send(cool()))
-  .get('/times', (req, res) => res.send(showTimes()))
-  
-  
+  .get('/postage', (req, res) => res.render('pages/postage'))
+  .get('/postage_amount', doCalcs)
   
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-  
-  showTimes = () => {
-  let result = ''
-  const times = process.env.TIMES || 5
-  for (i = 0; i < times; i++) {
-    result += i + ' '
-  }
-  return result;
+  function doMath(postalType, weight) 
+{
+	console.log(weight);console.log(postalType);
+	var price = 0;
+	if(postalType == "Letters (Stamped)")
+	{ if(weight == 1) {price = 0.50;}
+		else if (weight == 2) {price = 0.71;}
+		else if (weight ==3) {price = 0.92;}
+		else  if  (weight == 3.5) {price = 1.13;}	
+	}
+	
+	else if(postalType == "Letters (Metered)")
+	{
+		if(weight == 1) {price = 0.47;}
+		else if (weight == 2) {price = 0.68;}
+		else if (weight ==3) {price = 0.89;}
+		else  if  (weight == 3.5) {price = 1.10;}
+	}
+	else if(postalType == "Large Envelopes")
+	{
+		if(weight == 1) {price = 1.00;}
+		if(weight > 1) {price = (weight - 1) * .21 + 1;}
+	}
+	else if (postalType == "First Class Package")
+	{    if(weight <= 4) {price = 3.50;console.log("one");}
+		 else if(weight <= 8) {price = 3.75;console.log("two");}
+		 else if(weight <= 13) {price = (weight -8) * .35 + 3.75;console.log("three");}
+	}
+	
+	price = "$"+ price.toFixed(2)
+	console.log(price);
+	var JSONdata = {selection: postalType, weight: weight, payThis: price};
+	return JSONdata;
+}
+
+
+function doCalcs(request, response) {
+	var postalType = request.query.postageType
+	var weight = request.query.weight
+
+	var JSONdata = doMath(postalType, weight)
+	
+	response.render('pages/postage_amount', JSONdata)
 }
